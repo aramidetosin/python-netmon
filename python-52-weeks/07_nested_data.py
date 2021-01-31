dd308a7f13c10cf994beb7d7abcdbba0408bd939
Aramide Oluwatosin <aramide@pop-os.localdomain>
2021-01-31 13:35:58 +0000

    UPdate

---
+++ python-52-weeks/07_nested_data.py
@@ -0,0 +1,74 @@
+from pprint import pprint
+from random import choice
+import copy
+
+from utlis.create_utils import create_network
+
+device = {
+    "name": "r3-L-n7",
+    "vendor": "cisco",
+    "model": "catalyst 2960",
+    "os": "ios",
+    "interfaces": [
+
+    ]
+}
+
+print("\n\n----- device with no interfaces --------------------")
+for key, value in device.items():
+    print(f"{key:>16s} : {value}")
+
+interfaces = []
+
+for index in range(0, 8):
+    interface = {
+        'name': f'ge/0/0/{str(index)}',
+        'speed': choice(['10','100','1000'])
+    }
+    
+    interfaces.append(interface)
+    
+# pprint(interfaces)
+device['interfaces'] = interfaces
+
+print("\n\n----- device with interfaces --------------------")
+for key, value in device.items():
+    if key != 'interfaces':
+        print(f"{key:>16s} : {value}")
+    else:
+        print(f"{key:>16s} :")
+        for interface in interfaces:
+            print(f"\t\t\t\t\t{interface}")
+            
+print()
+print("\n\n----- device with interfaces using pprint--------------------")
+pprint(device)
+
+print("\n\n----- network with devices and interfaces --------------------")
+network = create_network(num_devices=4, num_subnets=4)
+pprint(network)
+
+print("\n----- information about network --------------------")
+print(f"-- number of subnets: {len(network['subnets'])}")
+print(f"-- list of subnets:   {network['subnets'].keys()}")
+print(f"-- list of subnets w/o extraneous: {', '.join(network['subnets'])}")
+
+print("\n----- network and devices nicely formatted --------------------")
+for subnet_address, subnet in network["subnets"].items():
+    print(f"\n-- subnet: {subnet_address}")
+    for device in subnet["devices"]:
+        print(f"   |-- device: {device['name']:8}  {device['ip']:10}  {device['vendor']:>10} : {device['os']}")
+
+print("\n\n----- remember assignment vs shallow copy vs deep copy --------------------")
+print("      modify 'network' only, and see if assign/copy/deepcopy versions reflect that change")
+network_assign = network
+network["subnets"]["10.0.1.0"]["devices"][0]["name"] = "different name assigned"
+print(f"  --- network == network_assign :    {network==network_assign}")
+
+network_copy = copy.copy(network)
+network["subnets"]["10.0.1.0"]["devices"][0]["name"] = "another different name, copy this time"
+print(f"  --- network == network_copy :      {network==network_copy}")
+
+network_deepcopy = copy.deepcopy(network)
+network["subnets"]["10.0.1.0"]["devices"][0]["name"] = "this time with deep copy"
+print(f"  --- network == network_deepcopy :  {network==network_deepcopy}")
--- python-52-weeks/utlis/create_utils.py
+++ python-52-weeks/utlis/create_utils.py
@@ -39,3 +39,66 @@ def create_devices(num_devices=1, num_subnets=1):
             created_device.append(device)
 
     return created_device
+
+
+def create_device(num_devices=1, num_subnets=1, random_ip=False):
+    
+    # CREATE LIST OF DEVICES
+    created_devices = list()
+
+    if num_devices > 254 or num_subnets > 254:
+        print("Error: too many devices and/or subnets requested")
+        return created_devices
+
+    print("beginning device creation")
+    for subnet_index in range(1, num_subnets+1):
+
+        for device_index in range(1, num_devices+1):
+
+            device = create_device(device_index, subnet_index, random_ip)
+            created_devices.append(device)
+
+    print("completed device creation")
+    return created_devices
+
+
+def create_network(num_devices=1, num_subnets=1):
+    
+    # We'll re-use the create_devices function we created earlier,
+    # then modify it to become an entire set of subnets, devices, and interfaces.
+
+    devices = create_devices(num_devices, num_subnets)
+
+    # Once again we are just going to consider the subnet to be the first three bytes
+    # of the IP address, just to simplify things for teaching
+
+    network = dict()
+    network["subnets"] = dict()
+
+    for device in devices:
+
+        # There are many ways to get the subnet address from an IP address,
+        # but for simplicity we are just going to replace the last byte with "0"
+        subnet_address_bytes = device["ip"].split(".")
+        subnet_address_bytes[3] = "0"
+        subnet_address = ".".join(subnet_address_bytes)
+
+        if subnet_address not in network["subnets"]:
+
+            network["subnets"][subnet_address] = dict()
+            network["subnets"][subnet_address]["devices"] = list()
+
+        network["subnets"][subnet_address]["devices"].append(device)
+
+        # Add interfaces to the device we just processed
+        interfaces = list()
+        for index in range(0, choice([2, 4, 8])):
+            interface = {
+                "name": "g/0/0/" + str(index),
+                "speed": choice(["10", "100", "1000"])
+            }
+            interfaces.append(interface)
+
+        device["interfaces"] = interfaces
+
+    return network
